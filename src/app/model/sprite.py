8c5f41a3b08cbceb0f21077d75ade693963d5452
paspallas <paspallasdev@gmail.com>
2022-12-31 20:52:08 +0100

    add opacity and tint properties to sprites

--- src/app/model/sprite.py
+++ src/app/model/sprite.py
@@ -1,4 +1,5 @@
-from PyQt5.QtGui import QColor, QPixmap
+from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtGui import QBrush, QColor, QPainter, QPixmap
 from PyQt5.QtWidgets import QGraphicsItem, QGraphicsPixmapItem, QWidget
 
 DEFAULT_ALPHA_MASK = "#FF00FF"
@@ -11,12 +12,14 @@ class Sprite(QGraphicsPixmapItem):
         self._pixmap = None
 
         self._setItemFlags()
-        self.setAcceptHoverEvents(True)
 
         self._setPixmapFromFile(path)
         self.setAlphaMask()
 
         self.setPixmap(self._pixmap)
+
+        self.setOpacity(50.0)
+        self.setTintColor(QColor(255, 0, 0, 100))
 
     def _setItemFlags(self):
         flags = (
@@ -27,6 +30,7 @@ class Sprite(QGraphicsPixmapItem):
         )
 
         self.setFlags(flags)
+        self.setAcceptHoverEvents(True)
 
     def _setPixmapFromFile(self, path: str):
         self._pixmap = QPixmap(path)
@@ -38,3 +42,24 @@ class Sprite(QGraphicsPixmapItem):
         self._pixmap.setMask(
             self._pixmap.createMaskFromColor(QColor(DEFAULT_ALPHA_MASK))
         )
+
+    def setOpacity(self, opacity: float) -> None:
+        if 0 <= opacity <= 100.0:
+            transparent = QPixmap(self._pixmap.size())
+            transparent.fill(Qt.transparent)
+
+            painter = QPainter(transparent)
+            painter.setOpacity(opacity * 0.01)
+            painter.drawPixmap(QPoint(), self._pixmap)
+            painter.end()
+
+            self._pixmap = transparent
+            self.setPixmap(self._pixmap)
+
+    def setTintColor(self, color: QColor) -> None:
+        painter = QPainter(self._pixmap)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceAtop)
+        painter.fillRect(self._pixmap.rect(), color)
+        painter.end()
+
+        self.setPixmap(self._pixmap)
