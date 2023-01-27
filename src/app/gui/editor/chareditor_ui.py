3b3319ef50d61f59e40f9aa5996ce7b6e1f59f16
paspallas <paspallasdev@gmail.com>
2023-01-27 17:39:11 +0100

    add multi sprite selection in the scene

--- src/app/gui/editor/chareditor_ui.py
+++ src/app/gui/editor/chareditor_ui.py
@@ -1,18 +1,25 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget
 
-from ..widget import CustomGraphicView, CustomGraphicViewOptions
-from .chareditorscene import CharEditorScene
+from ..widget import (
+    CustomGraphicScene,
+    CustomGraphicSceneOptions,
+    CustomGraphicView,
+    CustomGraphicViewOptions,
+)
 from .spritelistbox import SpriteListBox
 
 
 class CharEditorUi:
     def setupUi(self, parent: QWidget) -> None:
-        self.editorScene = CharEditorScene(parent)
+        self.editorScene = CustomGraphicScene(
+            parent, options=CustomGraphicSceneOptions(2048, 2048, 32, True)
+        )
+
         self.editorView = CustomGraphicView(
             self.editorScene,
             parent,
-            options=CustomGraphicViewOptions(False, False, True),
+            options=CustomGraphicViewOptions(True, False, True),
         )
 
         # widgets over the scene viewport
--- src/app/gui/widget/customgraphicscene.py
+++ src/app/gui/widget/customgraphicscene.py
@@ -18,10 +18,6 @@ class CustomGraphicSceneOptions(NamedTuple):
 
 
 class CustomGraphicScene(QGraphicsScene):
-
-    sigItemPositionChanged = pyqtSignal(QGraphicsItem)
-    sigItemSelectedChanged = pyqtSignal(QGraphicsItem)
-
     def __init__(self, *args, options: CustomGraphicSceneOptions, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -30,9 +26,6 @@ class CustomGraphicScene(QGraphicsScene):
         self.setSceneRect(
             -options.width / 2, -options.height / 2, options.width, options.height
         )
-
-        self._selectedItem = None
-        self._oldPosition = None
 
     @pyqtSlot(list)
     def addItems(self, items: list[QGraphicsItem]) -> None:
@@ -43,25 +36,6 @@ class CustomGraphicScene(QGraphicsScene):
     def delItems(self, items: list[QGraphicsItem]) -> None:
         for item in items:
             self.removeItem(item)
-
-    def mousePressEvent(self, e: QGraphicsSceneMouseEvent):
-        if e.button() == Qt.LeftButton:
-            items = self.items(e.buttonDownScenePos(Qt.LeftButton))
-            self._selectedItem = items[0] if items else None
-            if self._selectedItem:
-                self._oldPosition = self._selectedItem.pos()
-
-        self.clearSelection()
-        super().mousePressEvent(e)
-
-    def mouseReleaseEvent(self, e: QGraphicsSceneMouseEvent):
-        if self._selectedItem and e.button() == Qt.LeftButton:
-            if self._oldPosition != self._selectedItem.pos():
-                self.sigItemPositionChanged.emit(self._selectedItem)
-
-            self._selectedItem = None
-
-        super().mouseReleaseEvent(e)
 
     def drawBackground(self, painter: QPainter, rect: QRectF) -> None:
         painter.setRenderHint(QPainter.Antialiasing)
@@ -92,3 +66,4 @@ class CustomGraphicScene(QGraphicsScene):
             pen.setCosmetic(True)
             painter.setPen(pen)
             painter.drawLines(*lines)
+            painter.drawRect(QRectF(-160, -112, 320, 224))
